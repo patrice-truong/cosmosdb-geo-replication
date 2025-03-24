16b8a6dd82a1d582f25bd0d4906f027379832e48
patrice-truong <patrice-truong@users.noreply.github.com>
2025-03-24 13:36:40 +0000

    Update SOCKET_HOST IP address

--- nextjs/models/constants.ts
+++ nextjs/models/constants.ts
@@ -2,6 +2,6 @@
 export const userName = 'patrice.truong@gmail.com'
 export const api_url = 'http://localhost:4000'
 
-const SOCKET_HOST = process.env.SOCKET_HOST || '172.172.95.148'
+const SOCKET_HOST = process.env.SOCKET_HOST || '74.249.89.182'
 const SOCKET_PORT = process.env.SOCKET_PORT || '8000'
 export const socket_url = `http://${SOCKET_HOST}:${SOCKET_PORT}`
