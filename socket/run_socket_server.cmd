508423460212c0702489a5d1268258f86c896381
patrice-truong <patrice-truong@users.noreply.github.com>
2025-03-26 15:45:03 +0000

    removed unused files

--- socket/run_socket_server.cmd
+++ socket/run_socket_server.cmd
@@ -1,1 +1,1 @@
-npx ts-node src/server.ts
+npx ts-node server.ts
--- socket/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ChatBox from './components/ChatBox'
-
-export default function Home () {
-  return (
-    <div className='min-h-screen flex items-center justify-center p-4'>
-      <ChatBox />
-    </div>
-  )
-}
